51cabf53de39e4c6bd8ffdac3ef5a3029c3c67f3
Rezoan Ahmed <rezoan.cse.2017@gmail.com>
2023-09-10 01:02:33 +0600

    created Countries

---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h3>Countries</h3>
+        </div>
+    );
+};
+
+export default Countries;<h3>Countries</h3>
