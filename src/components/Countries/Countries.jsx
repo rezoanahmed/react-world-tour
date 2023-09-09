af0e0cbd12932190d358182d5f5d457585e67866
Rezoan Ahmed <rezoan.cse.2017@gmail.com>
2023-09-10 01:14:15 +0600

    showed country names using rest api

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import Country from '../Country/Country';
 
 const Countries = () => {
+    const [countries, setCountries]= useState([]);
+    useEffect(()=>{
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res=>res.json())
+        .then(data=>setCountries(data))
+    },[])
     return (
         <div>
-            <h3>Countries</h3>
+            <h2>Countries: {countries.length}</h2>
+            {
+                countries.map(element => <Country country={element}></Country>)
+            }
         </div>
     );
 };
 
-export default Countries;<h3>Countries</h3>
+export default Countries;
---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,11 @@
+const Country = ({country}) => {
+    // console.log(country);
+    const {name} = country;
+    return (
+        <div>
+            <h3>Name: {name?.common}</h3>
+        </div>
+    );
+};
+
+export default Country;
